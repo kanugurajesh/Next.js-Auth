ae23fcd87e98e542e328e0594f5a6ec414a4e354
Rajesh604 <kanugurajesh3@gmail.com>
2023-11-22 19:45:47 +0530

    Adding url token forget password endpoint

--- src/app/api/users/verifyforgetemail/route.ts
+++ src/app/api/users/verifyforgetemail/route.ts
@@ -11,23 +11,24 @@ export async function GET(request: NextRequest) {
         // parsing the request body into json
         const reqBody = await request.json();
         // getting the token from the request body
-        const { token } = reqBody;
+        const { token, password } = reqBody;
         // if no token is found in the request body return status 400 with a message
         if(!token) return NextResponse.json({error: 'No token found'},{status: 400});
+        if(!password) return NextResponse.json({error: 'No password found'},{status: 400});
         // find the user with the token and the token expiry is greater than the current time
         const user:any = User.findOne({forgotPasswordToken: token,forgotPasswordTokenExpiry: {$gt: Date.now()}})
         // if no user is found return status 400 with a message
         if (!user) return NextResponse.json({error: 'Invalid token'},{status: 400});
 
         // if user is found set the isVerified to true and remove the token and token expiry
-        user.isVerified = true;
         user.forgotPasswordToken = undefined;
         user.forgotPasswordTokenExpiry = undefined;
+        user.password = password;
         await user.save();
 
         // return status 200 with a message
         return NextResponse.json({
-                message: "Forget email verified successfully",
+                message: "Password reset successfully",
                 success: true
             })
             
--- src/app/verifyforgetemail/page.tsx
+++ src/app/verifyforgetemail/page.tsx
@@ -1,49 +1,73 @@
 "use client"
 
 import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
-import toast, { Toaster } from 'react-hot-toast';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import toast, {Toaster} from 'react-hot-toast';
 
 export default function VerifyEmailPage() {
 
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    const [password2, setPassword2] = useState<string>("");
+    const router = useRouter();
 
     const [token, setToken] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [password2, setPassword2] = useState<string>("");
     const [verified, setVerified] = useState<boolean>(false);
     const [error, setError] = useState<boolean>(false);
+    const [buttonDisabled, setButtonDisabled] = useState<boolean>(false);
+    const [showEye, setShowEye] = useState<boolean>(false);
+    const [showEye2, setShowEye2] = useState<boolean>(false);
 
-    const verifyForgetEmail = async () => {
+    useEffect(() => {
+        if(password && password2) {
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true);
+        }
+    },[password,password2])
+
+    const verifyUserEmail = async () => {
+
         try {
-            // sending a request to the verify-email api endpoint to verify the user
-            const response = await fetch(`/api/users/verifyforgetemail/${token}`, {
-                method: "GET",
+            // send a post request to the verify-email api endpoint to verify the user
+            const response = await fetch(`/api/users/verifyforgetemail`, {
+                method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
+                body: JSON.stringify({
+                    token: token,
+                    password: password
+                })
             });
-            
+          
             const data = await response.json();
 
             if (data.success) {
                 setVerified(true)
+            } else {
+                setError(true)
             }
-
+            
         } catch (error:any) {
             setError(true)
         }
     }
 
     useEffect(() => {
-        // taking the current url and splitting the url into two halves at = and taking the second value
         const urlToken = window.location.search.split("=")[1];
-        setToken(urlToken);
+        // if urlToken does not exist
+        if (!urlToken) {
+            toast.error("Invalid token");
+            router.push("/login")
+        } else {
+            setToken(urlToken);
+        }
     }, [])
 
     useEffect(() => {
-        if(token?.length) {
-            verifyForgetEmail();
+        if (token?.length > 0) {
+            verifyUserEmail();
         }
     }, [token])
 
@@ -51,18 +75,33 @@ export default function VerifyEmailPage() {
         <div className="flex flex-col justify-center h-screen items-center">
             <div className="flex flex-col gap-3 justify-center items-center">
                 <Toaster />
-                <h1 className="text-2xl font-bold">Enter your email</h1>
-                <label htmlFor="email"></label>
-                <input 
-                    type="email"
-                    id="email"
-                    value={email}
-                    onChange={(e)=>setEmail(e.target.value)}
-                    placeholder="email"
-                    className="h-10 w-60 border-2 border-black rounded-md px-3"
-                    />
+                <h1 className="text-2xl font-bold">Enter your password</h1>
+                <label htmlFor="password"></label>
+                <div className="relative">
+                    <input 
+                        type={showEye ? "text" : "password"}
+                        id="password"
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}
+                        placeholder="password"
+                        className="h-10 w-60 border-2 border-black rounded-md px-3"
+                        />
+                        <Image src={showEye ? "/view.png" : "/hide.png"} alt="" width={23} height={23} className="absolute right-3 top-2" onClick={()=>setShowEye(!showEye)}/>
+                </div>
+                <label htmlFor="password"></label>
+                <div className="relative">
+                    <input 
+                        type={showEye ? "text" : "password"}
+                        id="password"
+                        value={password2}
+                        onChange={(e)=>setPassword2(e.target.value)}
+                        placeholder="password"
+                        className="h-10 w-60 border-2 border-black rounded-md px-3"
+                        />
+                        <Image src={showEye2 ? "/view.png" : "/hide.png"} alt="" width={23} height={23} className="absolute right-3 top-2" onClick={()=>setShowEye2(!showEye2)}/>
+                </div>
                 <button className="h-10 w-40 border-2 border-black rounded-md bg-blue-500 hover:bg-blue-700 my-4 font-bold"
-                >verify email</button>
+                onClick={verifyUserEmail}>{buttonDisabled ? "Fill details" : "Reset Password"}</button>
             </div>
         </div>
     )
